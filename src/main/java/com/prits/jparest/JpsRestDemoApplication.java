a536cfe809d13c41ae5fde8c9248d2401c522f41
Pritesh Patel <3ppatel@gmail.com>
2016-09-02 10:57:45 -0400

    initial commit

---
+++ src/main/java/com/prits/jparest/JpsRestDemoApplication.java
@@ -0,0 +1,12 @@
+package com.prits.jparest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpsRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpsRestDemoApplication.class, args);
+	}
+}
